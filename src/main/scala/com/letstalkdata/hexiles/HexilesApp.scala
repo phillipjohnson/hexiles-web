cfe1ba8960e20f00c5af21333425085c2d811ece
Phillip Johnson <phillip@letstalkdata.com>
2015-04-30 19:22:33 -0400

    Board is drawn

--- src/main/scala/com/letstalkdata/hexiles/HexilesApp.scala
+++ src/main/scala/com/letstalkdata/hexiles/HexilesApp.scala
@@ -15,33 +15,24 @@ object HexilesApp extends JSApp {
   val canvas = document.getElementById("game-canvas").asInstanceOf[dom.raw.HTMLCanvasElement]
   val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
   def main() = {
-    val h = new Hexagon(0,0)
-    val p = h.centerPoint(50)
 
-    val points = h.perimeterPoints(p,50)
-
-    ctx.beginPath()
-    (0 until 6).foreach(i => {
-      val start = points(i)
-      val end = points((i + 1) % 6)
-      ctx.moveTo(start.x, start.y)
-      ctx.lineTo(end.x, end.y)
+    val hexes:Seq[Hexagon] = (0 to 4).flatMap(row => {
+      (4 - row to 8 - row).map(column => new Hexagon(column, row))
     })
-    ctx.stroke()
 
-    val h2 = new Hexagon(1,1)
-    val p2 = h2.centerPoint(50)
-
-    val points2 = h2.perimeterPoints(p2, 50)
-
-    ctx.beginPath()
-    (0 until 6).foreach(i => {
-      val start = points2(i)
-      val end = points2((i + 1) % 6)
-      ctx.moveTo(start.x, start.y)
-      ctx.lineTo(end.x, end.y)
+    hexes.foreach(hex => {
+      val center = hex.centerPoint(35)
+      val points = hex.perimeterPoints(center, 35)
+      ctx.beginPath()
+      (0 until 6).foreach(i => {
+        val start = points(i)
+        val end = points((i + 1) % 6)
+        ctx.moveTo(start.x, start.y)
+        ctx.lineTo(end.x, end.y)
+      })
+      ctx.stroke()
     })
-    ctx.stroke()
 
   }
+
 }
--- src/main/scala/com/letstalkdata/hexiles/shapes/Hexagon.scala
+++ src/main/scala/com/letstalkdata/hexiles/shapes/Hexagon.scala
@@ -12,24 +12,24 @@ object Hexagon {
 }
 
 
-class Hexagon(val q: Int, val r:Int) {
+class Hexagon(val column: Int, val row:Int) {
 
 
-  val cube = new Cube(q, -q - r, r)
+  val cube = new Cube(column, -column - row, row)
 
   def perimeterPoints(center:Point, size:Float):Seq[Point] = {
-    (0 to 5).map(i => new Point(
+    (0 until 6).map(i => new Point(
       center.x + size * math.cos(Hexagon.RotationAngle * i).toFloat,
       center.y - size * math.sin(Hexagon.RotationAngle * i).toFloat))
   }
 
   def perimeter(center:Point, size:Float):Seq[Segment] = {
     val points = perimeterPoints(center, size)
-    (0 to 5).map(i => new Segment(points(i), points((i + 1) % 6)))
+    (0 until 6).map(i => new Segment(points(i), points((i + 1) % 6)))
   }
 
   def centerPoint(radius:Float):Point = {
-    new Point(radius * 3/2 * q, radius * Hexagon.Sqrt3 * (r + q/2))
+    new Point(radius * 1.5f * column, radius * Hexagon.Sqrt3 * (row + column/2.0f))
   }
 
 
